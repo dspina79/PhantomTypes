3e67e92f3c8d2bef6556ae300a70a6f5b677b8ce
Dave Spina <dspina79@gmail.com>
2021-03-03 21:07:53 -0500

    Added rudimentary state machine and undefined

--- PhantomTypesIntro/PhantomTypesIntro/main.swift
+++ PhantomTypesIntro/PhantomTypesIntro/main.swift
@@ -7,6 +7,7 @@
 
 import Foundation
 
+/*
 // phantom type that does not use one of its generic parameters
 // different than regular generic types
 struct Employee<Role>: Equatable {
@@ -41,5 +42,52 @@ let sample3 = BloodSample<OPositive>(amount: 10)
 
 let combined1 = sample1 + sample3 //valid
 //let combined2 = sample1 + sample2 // invalid
+
+*/
+
+/*
+ ========= State Machine Example =========
+ */
+
+enum Waiting {}
+enum CoinInserted {}
+enum Fetching {}
+enum Dispensing {}
+
+struct Transition <From, To> {}
+
+struct Machine<State> {
+    func transition<To>(from transition: Transition<State, To>) -> Machine<To> {
+        .init()
+    }
+}
+
+
+let starting = Transition<Waiting, CoinInserted>()
+let selection = Transition<CoinInserted, Fetching>()
+let delivery = Transition<Fetching, Dispensing>()
+let reset = Transition<Dispensing, Waiting>()
+
+
+let m1 = Machine<Waiting>()
+let m2 = m1.transition(from: starting)
+let m3 = m2.transition(from: selection)
+let m4 = m3.transition(from: delivery)
+let m5 = m4.transition(from: reset)
+
+//let m6 = m3.transition(from: reset) // invalid
+
+// a goood little function to have that is not a phantom type
+// but takes advantage of the SwiftUI type structure
+
+func undefined<T>(_ message: String = "") -> T {
+    fatalError("Undefined: \(message)")
+}
+
+func methodNotYetReady(name: String) -> Int? {
+    undefined(name)
+}
+
+
 
 
